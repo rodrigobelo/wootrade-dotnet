aa83b323437d394ac97f868945515bfd723cd43c
rodrigobelo <rodrigobelo1@gmail.com>
2021-07-22 22:56:48 +0100

    chore: remove comments

--- src/Wootrade/Model/MarketData/WootradeSymbol.cs
+++ src/Wootrade/Model/MarketData/WootradeSymbol.cs
@@ -5,19 +5,6 @@ namespace Wootrade.Model.MarketData
 {
     public class WootradeSymbol : ICommonSymbol
     {
-        /* "created_time": "1575441595.65",
-      "updated_time": "1575441595.65",
-      "symbol": "SPOT_BTC_USDT",
-      "quote_min": 100,
-      "quote_max": 100000,
-      "quote_tick": 0.01,
-      "base_min": 0.0001,
-      "base_tick": 0.0001,
-      "min_notional": 0.02,
-      "price_range": 0.99
-         *
-         * */
-
         public decimal CommonMinimumTradeSize => MinimumQuote;
 
         public string CommonName => Name;
